e8cfc26ab2f9d65180faff8af193ed143ee19199
ref <56499429+referefref@users.noreply.github.com>
2024-01-02 23:00:00 +0800

    Version 0.9.128
    Major refactoring of js code to make use of new data structure from backend
    Bug fixes
    Search function
    Exception handling

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,29 @@
 var storedScans = {};
+var hasNewScans = false;
 
 document.addEventListener('DOMContentLoaded', function () {
-    if (document.getElementById("searchInput")) {
-        document.getElementById("searchInput").addEventListener("keyup", function() {
+    fetchScans();
+    setInterval(function() {
+        if (hasNewScans) {
+            fetchScans();
+        }
+    }, 1000);
+
+    var scanBtn = document.getElementById('scanBtn');
+    if (scanBtn) {
+        scanBtn.addEventListener('click', startScan);
+    } else {
+        console.error("Scan button not found");
+    }
+
+    var searchInput = document.getElementById('searchInput');
+    if (searchInput) {
+        searchInput.addEventListener('keyup', function() {
             var search = this.value.toLowerCase();
-            $("#resultsTable tbody tr").filter(function() {
-                $(this).toggle($(this).text().toLowerCase().indexOf(search) > -1)
-            });
+            filterTableRows('resultsTable', search);
         });
+    } else {
+        console.error("Search input not found");
     }
     
     var clearBtn = document.getElementById("clearBtn");
@@ -27,6 +43,23 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     
 });
+
+function filterTableRows(tableId, search) {
+    var table = document.getElementById(tableId);
+    var tr = table.getElementsByTagName('tr');
+
+    for (var i = 1; i < tr.length; i++) {
+        var td = tr[i].getElementsByTagName('td');
+        var found = false;
+        for (var j = 0; j < td.length; j++) {
+            if (td[j].textContent.toLowerCase().indexOf(search) > -1) {
+                found = true;
+                break;
+            }
+        }
+        tr[i].style.display = found ? '' : 'none';
+    }
+}
 
 function toggleAdvancedOptions() {
     var advancedOptions = document.getElementById("advancedOptions");
@@ -61,95 +94,243 @@ function startScan() {
     var scanId = host + ":" + port + ":" + new Date().getTime();
     var scanTime = new Date().toLocaleString();
 
-    addScanToExecutionList(scanId, host, port, scanTime);
-
-    axios.get(`/scan?${params}`)
-    .then(response => {
-        const data = response.data;
-        // Store the scan results
-        storedScans[scanId] = data;
-        markScanAsCompleted(scanId, host, port);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        markScanAsError(scanId, host, port);
+    addScanToExecutionList({
+        scan_id: scanId,
+        start_time: new Date().toLocaleString(),
+        hosts: host.split(','),
+        ports: port.split(','),
+        status: 'New'
     });
+
+	axios.get(`/scan?${params}`)
+        .then(response => {
+            const scanId = response.data.scan_id;
+            console.log("Scan started. ScanId:", scanId);
+
+            addScanToExecutionList({
+                scan_id: scanId,
+                start_time: new Date().toLocaleString(),
+                host: host.split(','),
+                port: port.split(','),
+                status: 'New'
+            });
+            storedScans[scanId] = {
+                hosts: host.split(','),
+                ports: port.split(','),
+                status: 'New',
+                results: []
+            };
+
+            hasNewScans = true;
+            pollForScanCompletion(scanId);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 }
 
-function addScanToExecutionList(scanId, host, port, scanTime) {
-    var tbody = document.getElementById("scanExecutionsTable").getElementsByTagName("tbody")[0];
-    
-    if (tbody) {
-        var row = tbody.insertRow();
-        var startTimeCell = row.insertCell(0);
-	var finishTimeCell = row.insertCell(1);
-        var targetsCell = row.insertCell(2);
-        var portsCell = row.insertCell(3);
-        var statusCell = row.insertCell(4);
-        var actionCell = row.insertCell(5);
+function pollForScanCompletion(scanId) {
+    const checkCompletion = setInterval(() => {
+        axios.get(`/getScans`)
+            .then(response => {
+                const scan = response.data.find(s => s.scan_id === scanId);
+                if (scan && scan.end_time) {
+                    clearInterval(checkCompletion);
+                    storedScans[scanId] = scan;
+                    updateScanExecutionList(scan);
+                    console.log("Scan completed. ScanId:", scanId, "Final Scan Data:", scan);
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching scan status:', error);
+                clearInterval(checkCompletion);
+            });
+    }, 1000);
+}
 
-        startTimeCell.innerHTML = scanTime;
-        targetsCell.innerHTML = host;
-        portsCell.innerHTML = port;
-        finishTimeCell.innerHTML = ''; // Initially empty, filled on scan completion
-        statusCell.innerHTML = 'New';
-        actionCell.innerHTML = ''; // Initially empty, will be filled later
-
-        row.dataset.scanId = scanId; // Ensure this is correctly being set
-        row.dataset.scanTime = scanTime; // Store scan time    
-
+function updateScanExecutionList(scan) {
+    var row = document.querySelector(`#scanExecutionsTable tbody tr[data-scan-id="${scan.scan_id}"]`);
+    if (row) {
+        row.cells[1].innerHTML = scan.end_time ? new Date(scan.end_time).toLocaleString() : 'Running';
+        row.cells[2].innerHTML = scan.target_hosts ? scan.hosts.join(", ") : 'N/A'; 
+        row.cells[3].innerHTML = scan.target_ports ? scan.ports.join(", ") : 'N/A'; 
+	row.cells[4].innerHTML = 'Completed';
+        row.cells[5].innerHTML = getActionButtonsHTML(scan.scan_id);
     } else {
-        console.error("Execution list table not found");
+        addScanToExecutionList(scan);
     }
 }
 
-function markScanAsCompleted(scanId, host, port) {
-    var row = document.querySelector(`#scanExecutionsTable tbody tr[data-scan-id="${scanId}"]`);
-    if (row) {
-        var finishTimeCell = row.cells[1];
-        var statusCell = row.cells[4];
-        var actionCell = row.cells[5];
+function addScanToExecutionList(scan) {
+    var tbody = document.getElementById("scanExecutionsTable").getElementsByTagName("tbody")[0];
+    var row = tbody.insertRow();
+    if (tbody) {
+	    row.dataset.scanTime = scan.start_time;
+    }
+    var startTimeCell = row.insertCell(0);
+    var finishTimeCell = row.insertCell(1);
+    var targetsCell = row.insertCell(2);
+    var portsCell = row.insertCell(3);
+    var statusCell = row.insertCell(4);
+    var actionCell = row.insertCell(5);
 
-        finishTimeCell.innerHTML = new Date().toLocaleString();
-        statusCell.innerHTML = 'Completed';
-        actionCell.innerHTML = '<button class="btn btn-primary btn-sm" onclick="viewResults(\'' + scanId + '\')">View Results</button>' +
-                                 '<button class="btn btn-success btn-sm ml-2" onclick="downloadResults(\'' + scanId + '\', \'json\')">JSON</button>' +
-                                 '<button class="btn btn-success btn-sm ml-2" onclick="downloadResults(\'' + scanId + '\', \'csv\')">CSV</button>';
+    startTimeCell.innerHTML = new Date(scan.start_time).toLocaleString();
+    finishTimeCell.innerHTML = scan.end_time ? new Date(scan.end_time).toLocaleString() : '';
+    targetsCell.innerHTML = scan.hosts ? scan.hosts.join(", ") : 'N/A';
+    portsCell.innerHTML = scan.ports ? scan.ports.join(", ") : 'N/A';
+    statusCell.innerHTML = scan.status;
+
+    if (statusCell.innerHTML === 'Completed') {
+        actionCell.innerHTML = getActionButtonsHTML(scan.scan_id);
+    } else {
+        actionCell.innerHTML = '';
+    }
+
+    row.dataset.scanId = scan.scan_id;
+}
+
+function fetchScans() {
+    axios.get('/getScans')
+        .then(response => {
+            const scans = response.data;
+
+            if (!scans || !Array.isArray(scans)) {
+                console.error('Invalid scan data received:', scans);
+                return; 
+            }
+
+            clearScanExecutionsTable();
+            scans.forEach(scan => {
+                storedScans[scan.scan_id] = {
+                    hosts: scan.hosts,
+                    ports: scan.ports,
+                    results: scan.results,
+                    status: scan.end_time ? 'Completed' : 'New'
+                };
+                addScanToExecutionList(scan);
+            });
+            autoRefreshForNewScans();
+        })
+        .catch(error => {
+            console.error('Error fetching scans:', error);
+        });
+}
+
+
+function aggregateScans(scanResults) {
+    const aggregatedScans = {};
+    scanResults.forEach(result => {
+        if (!aggregatedScans[result.scan_id]) {
+            aggregatedScans[result.scan_id] = {
+                scan_id: result.scan_id,
+                start_time: result.start_time,
+                end_time: result.end_time,
+                hosts: new Set(),  
+                ports: new Set(),
+                results: []
+            };
+        }
+        aggregatedScans[result.scan_id].hosts.add(result.host);
+        aggregatedScans[result.scan_id].ports.add(result.port.toString());
+        aggregatedScans[result.scan_id].results.push(result);
+    });
+
+    Object.values(aggregatedScans).forEach(scan => {
+        scan.hosts = Array.from(scan.hosts);
+        scan.ports = Array.from(scan.ports);
+    });
+
+    return aggregatedScans;
+}
+
+function clearScanExecutionsTable() {
+    var table = document.getElementById("scanExecutionsTable").getElementsByTagName("tbody")[0];
+    table.innerHTML = ''; 
+}
+
+function addScanToExecutionList(scan) {
+    var tbody = document.getElementById("scanExecutionsTable").getElementsByTagName("tbody")[0];
+    var row = tbody.insertRow();
+
+    var startTimeCell = row.insertCell(0);
+    var finishTimeCell = row.insertCell(1);
+    var targetsCell = row.insertCell(2);
+    var portsCell = row.insertCell(3);
+    var statusCell = row.insertCell(4);
+    var actionCell = row.insertCell(5);
+
+    startTimeCell.innerHTML = scan.start_time ? new Date(scan.start_time).toLocaleString() : 'Unknown Time';
+    finishTimeCell.innerHTML = scan.end_time ? new Date(scan.end_time).toLocaleString() : '';
+    targetsCell.innerHTML = scan.target_hosts ? scan.target_hosts : 'N/A';
+    portsCell.innerHTML = scan.target_ports ? scan.target_ports : 'N/A';
+    statusCell.innerHTML = scan.results && scan.results.length > 0 ? 'Completed' : 'New';
+
+   if (statusCell.innerHTML === 'Completed') {
+        actionCell.innerHTML = '<button class="btn btn-primary btn-sm" onclick="viewResults(\'' + scan.scan_id + '\')">View Results</button>' +
+                               '<button class="btn btn-success btn-sm ml-2" onclick="downloadResults(\'' + scan.scan_id + '\', \'json\')">JSON</button>' +
+                               '<button class="btn btn-success btn-sm ml-2" onclick="downloadResults(\'' + scan.scan_id + '\', \'csv\')">CSV</button>';
+    } else {
+        actionCell.innerHTML = '';
+    }
+
+    row.dataset.scanId = scan.scan_id;
+}
+
+function getActionButtonsHTML(scanId) {
+    return '<button class="btn btn-primary btn-sm" onclick="viewResults(\'' + scan.scan_id + '\')">View Results</button>' +
+           '<button class="btn btn-success btn-sm ml-2" onclick="downloadResults(\'' + scan.scan_id + '\', \'json\')">JSON</button>' +
+           '<button class="btn btn-success btn-sm ml-2" onclick="downloadResults(\'' + scan.scan_id + '\', \'csv\')">CSV</button>';
+}
+
+function autoRefreshForNewScans() {
+    const scans = document.querySelectorAll('#scanExecutionsTable tbody tr');
+    const hasNewScans = Array.from(scans).some(row => row.cells[4].innerHTML === 'New');
+
+    if (hasNewScans) {
+        setTimeout(fetchScans, 1000);
     }
 }
 
 function viewResults(scanId) {
-    if (storedScans[scanId]) {
-        populateResults(storedScans[scanId], scanId);
+    var scan = storedScans[scanId];
+    if (scan && scan.results) {
+        populateResults(scan.results, scanId);
     } else {
-        console.error('No data found for scanId:', scanId);
+        console.error('No data or invalid data found for scanId:', scanId);
     }
 }
 
-
-function populateResults(data, scanId) {
+function populateResults(results, scanId) {
     var tbody = document.getElementById("resultsTable").getElementsByTagName("tbody")[0];
-    tbody.innerHTML = ''; // Clear current results
+    tbody.innerHTML = ''; 
 
-    var scanRow = document.querySelector(`#scanExecutionsTable tbody tr[data-scan-id="${scanId}"]`);
-    var scanTime = scanRow ? scanRow.dataset.scanTime : 'Unknown Time';
-
-    data.forEach(function(result) {
+    results.forEach(result => {
         var row = tbody.insertRow();
-        var scanTimeCell = row.insertCell(0);
+        var detectionTimeCell = row.insertCell(0);
         var hostCell = row.insertCell(1);
         var portCell = row.insertCell(2);
         var isHoneypotCell = row.insertCell(3);
         var honeypotTypeCell = row.insertCell(4);
 
-        scanTimeCell.innerHTML = scanTime;
-	hostCell.innerHTML = result.Host;
-        portCell.innerHTML = result.Port;
-        isHoneypotCell.innerHTML = result.IsHoneypot ? 'Yes' : 'No';
-        honeypotTypeCell.innerHTML = result.HoneypotType;
+        detectionTimeCell.innerHTML = result.detection_time ? new Date(result.detection_time).toLocaleString() : 'N/A';
+        hostCell.innerHTML = result.host || 'N/A';
+        portCell.innerHTML = result.port || 'N/A';
+        isHoneypotCell.innerHTML = result.is_honeypot ? 'Yes' : 'No';
+        honeypotTypeCell.innerHTML = result.honeypot_type || 'N/A';
 
-        row.className = result.IsHoneypot ? 'alert-honeypot' : 'alert-nonhoneypot';
+        row.className = result.is_honeypot ? 'alert-honeypot' : 'alert-nonhoneypot';
     });
+}
+
+function extractScanTime(scanId) {
+    var scan = storedScans[scanId];
+    console.log("Extracting time for scanId:", scanId, "Data:", scan);
+
+    if (scan && scan.start_time) {
+        return new Date(scan.start_time).toLocaleString();
+    } else {
+        return 'Unknown Time';
+    }
 }
 
 function clearResults() {
@@ -170,9 +351,9 @@ function downloadResults(scanId, format) {
             dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(data));
         } else if (format === 'csv') {
             let csvContent = "data:text/csv;charset=utf-8,";
-            csvContent += "Scan Time,Host,Port,Is Honeypot,Honeypot Type\n"; // CSV Header
+            csvContent += "Scan Time,Host,Port,Is Honeypot,Honeypot Type\n"; 
 
-            const scanTime = new Date(parseInt(scanId.split(':')[2])).toISOString().replace(/T/, ' ').replace(/\..+/, ''); // Extract and format time from scanId
+            const scanTime = new Date(parseInt(scanId.split(':')[2])).toISOString().replace(/T/, ' ').replace(/\..+/, ''); 
 
             data.forEach(row => {
                 const rowArray = [
@@ -191,13 +372,12 @@ function downloadResults(scanId, format) {
             return;
         }
 
-        // Create a download element and trigger download
         const downloadElement = document.createElement('a');
         downloadElement.setAttribute('href', dataStr);
         downloadElement.setAttribute('download', `scan_results_${scanId}.${format}`);
-        document.body.appendChild(downloadElement); // Append to body
-        downloadElement.click(); // Simulate click to trigger download
-        document.body.removeChild(downloadElement); // Remove the element after download
+        document.body.appendChild(downloadElement); 
+        downloadElement.click(); 
+        document.body.removeChild(downloadElement); 
     } else {
         console.error("No data found for scanId:", scanId);
     }
