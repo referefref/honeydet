3d4178b7862eb766202a4126f9c58c016cf65819
ref <56499429+referefref@users.noreply.github.com>
2024-01-11 15:05:28 +0800

    Create report.go

---
+++ report.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"github.com/TwiN/go-color"
+	_ "github.com/mattn/go-sqlite3"
+	"os"
+)
+
+func generateReport(results []DetectionResult, reportType string) {
+	switch reportType {
+	case "json":
+		reportJSON(results)
+	case "csv":
+		reportCSV(results)
+	default:
+		if *verbose {
+			fmt.Println(color.Ize(color.Red, "[Verbose] Invalid report type specified. No report generated."))
+		}
+	}
+}
+
+func reportJSON(results []DetectionResult) {
+	data, err := json.Marshal(results)
+	if err != nil {
+		fmt.Println(color.Ize(color.Red, ("Error generating JSON report:" + err.Error())))
+		return
+	}
+	fmt.Println(string(data))
+}
+
+func reportCSV(results []DetectionResult) {
+	writer := csv.NewWriter(os.Stdout)
+	defer writer.Flush()
+	writer.Write([]string{"Host", "IsHoneypot", "HoneypotType"})
+
+	for _, result := range results {
+		record := []string{result.Host, fmt.Sprintf("%t", result.IsHoneypot), result.HoneypotType}
+		writer.Write(record)
+	}
+}
