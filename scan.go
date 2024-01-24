b86ab961637c8c567260def9d09f64ee02a108db
ref <56499429+referefref@users.noreply.github.com>
2024-01-24 17:57:15 +0800

    Update scan.go
    Major changes to scan logic allowing for connection persistence with multi-step signatures

--- scan.go
+++ scan.go
@@ -7,8 +7,11 @@ import (
 	"github.com/gosnmp/gosnmp"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/simonvetter/modbus"
+	"github.com/tomsteele/go-shodan"
 	"golang.org/x/crypto/ssh"
+	"io"
 	"net"
+	"net/url"
 	"os/exec"
 	"regexp"
 	"strconv"
@@ -16,36 +19,44 @@ import (
 	"time"
 )
 
+func isPublicIP(ip string) bool {
+	parsedIP := net.ParseIP(ip)
+	if parsedIP.IsLoopback() || parsedIP.IsPrivate() {
+		return false
+	}
+	return true
+}
+
 func connectToNetworkService(host string, port int, proto string, timeout int) (net.Conn, error) {
 	address := fmt.Sprintf("%s:%d", host, port)
 	var conn net.Conn
 	var err error
 
 	if *verbose {
-		fmt.Printf("[Verbose] Attempting to connect to %s:%d over %s with timeout %d seconds\n", host, port, proto, timeout)
+		fmt.Println(color.Ize(color.Purple, fmt.Sprintf("[Verbose] Attempting to connect to %s:%d over %s with timeout %d seconds", host, port, proto, timeout)))
 	}
 
 	switch proto {
 	case "tcp", "udp":
 		conn, err = net.DialTimeout(proto, address, time.Duration(timeout)*time.Second)
 		if err != nil {
-			if *verbose {
-				fmt.Printf("[Verbose] Failed to connect: %s\n", err)
+			if *debug {
+				fmt.Printf("[Debug] Failed to connect: %s", err)
 			}
 			return nil, err
 		}
 		deadline := time.Now().Add(time.Duration(timeout) * time.Second)
 		if err := conn.SetDeadline(deadline); err != nil {
-			if *verbose {
-				fmt.Printf("[Verbose] Failed to set deadline: %s\n", err)
+			if *debug {
+				fmt.Printf("[Debug] Failed to set deadline: %s", err)
 			}
 			conn.Close()
 			return nil, err
 		}
 	default:
 		err = fmt.Errorf("unsupported protocol: %s", proto)
-		if *verbose {
-			fmt.Printf("[Verbose] Unsupported protocol: %s\n", proto)
+		if *debug {
+			fmt.Printf("[Debug] Unsupported protocol: %s", proto)
 		}
 		return nil, err
 	}
@@ -55,7 +66,7 @@ func connectToNetworkService(host string, port int, proto string, timeout int) (
 
 func hostRespondsToPing(host string, timeout int) bool {
 	if *verbose {
-		fmt.Printf("[Verbose] Checking if host %s responds to ping\n", host)
+		fmt.Println(color.Ize(color.Green, fmt.Sprintf("[Verbose] Checking if host %s responds to ping", host)))
 	}
 
 	cmd := exec.Command("ping", "-c", "1", "-W", strconv.Itoa(timeout), host)
@@ -63,28 +74,41 @@ func hostRespondsToPing(host string, timeout int) bool {
 	return err == nil
 }
 
-func detectHoneypot(host string, ports []int, proto string, signatures Signatures, timeout int, bypassPortCheck bool) []DetectionResult {
+func detectHoneypot(host string, ports []int, proto string, signatures Signatures, timeout int, bypassPortCheck bool, shodanClient *shodan.Client) []DetectionResult {
 	var results []DetectionResult
 	detectionTime := time.Now()
 
+	var shodanInfo *shodan.Host
+	if *useShodan {
+		if shodanClient != nil && isPublicIP(host) {
+			var err error
+			opts := url.Values{}
+			shodanInfo, err = shodanClient.Host(host, opts)
+			if err != nil {
+				if *debug {
+					fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Debug]Error fetching Shodan data for host %s: %s", host, err)))
+				}
+			}
+		}
+	}
+
 	for _, port := range ports {
-		var conn net.Conn
-		var err error
 		var sshClient *ssh.Client
 		var modbusClient *modbus.ModbusClient
-		var isSSH, isModbus, honeypotDetected bool
-		var honeypotType string = ""
+		var isSSH, isModbus bool
+		var err error
 
 		if *username != "" && *password != "" && proto == "tcp" && port != 161 {
 			isSSH = probeSSHServer(host, port, timeout)
+
 			if isSSH {
-				if *verbose {
-					fmt.Println(color.Ize(color.Blue, fmt.Sprintf("[Verbose] SSH service detected on host %s:%d, attempting authentication", host, port)))
+				if *debug {
+					fmt.Println(color.Ize(color.Purple, fmt.Sprintf("[Debug] SSH service detected on host %s:%d, attempting authentication", host, port)))
 				}
 				sshClient, err = authenticateSSH(host, port, *username, *password, timeout)
 				if err != nil {
-					if *verbose {
-						fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Verbose] SSH authentication failed for host %s:%d: %s", host, port, err)))
+					if *debug {
+						fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Debug] SSH authentication failed for host %s:%d: %s", host, port, err)))
 					}
 					continue
 				}
@@ -94,8 +118,8 @@ func detectHoneypot(host string, ports []int, proto string, signatures Signature
 			isModbus = true
 			modbusClient, err := setupModbusClient(host, port, time.Duration(timeout)*time.Second)
 			if err != nil {
-				if *verbose {
-					fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Verbose] Error setting up Modbus client for host %s: %s", host, err)))
+				if *debug {
+					fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Debug] Error setting up Modbus client for host %s: %s", host, err)))
 				}
 				continue
 			}
@@ -106,37 +130,13 @@ func detectHoneypot(host string, ports []int, proto string, signatures Signature
 		if port == 161 {
 			snmpClient, err = setupSNMPClient(host, timeout)
 			if err != nil {
-				if *verbose {
-					fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Verbose] Error setting up SNMP client for host %s: %s", host, err)))
+				if *debug {
+					fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Debug] Error setting up SNMP client for host %s: %s", host, err)))
 				}
 				continue
 			}
 			defer snmpClient.Conn.Close()
 		}
-
-		if !isSSH && port != 161 && port != 502 {
-			if *verbose {
-				fmt.Println(color.Ize(color.White, fmt.Sprintf("[Verbose] Establishing regular %s connection to host %s:%d", proto, host, port)))
-			}
-		}
-		conn, err = connectToNetworkService(host, port, proto, timeout)
-		if err != nil {
-			if *verbose {
-				fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Verbose] Unable to connect to host %s:%d: %s", host, port, err)))
-			}
-			result := DetectionResult{
-				Host:          host,
-				Port:          port,
-				IsHoneypot:    false,
-				HoneypotType:  "",
-				Confidence:    "High",
-				Comment:       fmt.Sprintf("Host did not respond to %s connection", proto),
-				DetectionTime: detectionTime,
-			}
-			results = append(results, result)
-			continue // Skip to next port
-		}
-		defer conn.Close()
 
 		for _, signature := range signatures.Signatures {
 			if bypassPortCheck || isPortMatch(port, signature.Port) {
@@ -144,13 +144,26 @@ func detectHoneypot(host string, ports []int, proto string, signatures Signature
 					fmt.Println(color.Ize(color.Green, fmt.Sprintf("[Verbose] Testing signature '%s' on host %s:%d", signature.Name, host, port)))
 				}
 
+				var conn net.Conn
+				if !isSSH && !isModbus && port != 161 {
+					conn, err = connectToNetworkService(host, port, proto, timeout)
+					if err != nil {
+						if *debug {
+							fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Debug] Unable to connect to host %s:%d: %s", host, port, err)))
+						}
+						continue
+					}
+					defer conn.Close()
+				}
+
+				signatureMatched := true
 				for _, step := range signature.Steps {
 					var response string
 					if port == 161 {
 						response, err = sendSNMPRequest(snmpClient, step)
 						if err != nil {
-							if *verbose {
-								fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Verbose] SNMP request failed for signature '%s' on host %s:%d: %s", signature.Name, host, port, err)))
+							if *debug {
+								fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Debug] SNMP request failed for signature '%s' on host %s:%d: %s", signature.Name, host, port, err)))
 							}
 							break
 						}
@@ -158,8 +171,8 @@ func detectHoneypot(host string, ports []int, proto string, signatures Signature
 						var modbusResponse string
 						modbusResponse, err := sendModbusRequest(modbusClient, step)
 						if err != nil {
-							if *verbose {
-								fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Verbose] Modbus request failed for signature '%s' on host %s:%d: %s", signature.Name, host, port, err)))
+							if *debug {
+								fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Debug] Modbus request failed for signature '%s' on host %s:%d: %s", signature.Name, host, port, err)))
 							}
 							break
 						}
@@ -167,167 +180,201 @@ func detectHoneypot(host string, ports []int, proto string, signatures Signature
 					} else if isSSH && sshClient != nil {
 						response, err = executeSSHCommand(sshClient, step.Input)
 						if err != nil {
-							if *verbose {
-								fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Verbose] Error executing SSH command: %s", err)))
+							if *debug {
+								fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Debug] Error executing SSH command: %s", err)))
 							}
 							break
 						}
 					} else {
-						conn, err = sendRequest(host, port, step, timeout)
+						err = sendRequest(conn, step, timeout)
 						if err != nil {
-							if *verbose {
-								fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Verbose] Error sending request: %s", err)))
+							if *debug {
+								fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Debug] Error sending request: %s", err)))
 							}
+							signatureMatched = false
 							break
 						}
 						response, err = readResponse(conn)
+						if err != nil {
+							if *debug {
+								fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Debug] Error reading response: %s", err)))
+							}
+							signatureMatched = false
+							break
+						}
 					}
 
-					if isResponseMatch(response, step.OutputMatchType, step.Output) {
-						honeypotDetected = true
-						honeypotType = signature.Name
-						if *verbose {
-							fmt.Println(color.Ize(color.Green, fmt.Sprintf("[Verbose] Match found for signature '%s'", signature.Name)))
+					invertMatch, err := strconv.ParseBool(step.InvertMatch)
+					if err != nil {
+						if *debug {
+							fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Debug] Error parsing InvertMatch: %s", err)))
 						}
+						continue
+					}
+					if !isResponseMatch(response, step.OutputMatchType, step.Output, invertMatch) {
+						signatureMatched = false
 						break
 					}
 				}
-			}
 
-			if honeypotDetected {
-				result := DetectionResult{
-					Host:          host,
-					Port:          port,
-					IsHoneypot:    true,
-					HoneypotType:  honeypotType,
-					Confidence:    signature.Confidence,
-					Comment:       signature.Comment,
-					DetectionTime: detectionTime,
+				if signatureMatched {
+					result := DetectionResult{
+						Host:          host,
+						Port:          port,
+						IsHoneypot:    true,
+						HoneypotType:  signature.Name,
+						Confidence:    signature.Confidence,
+						Comment:       signature.Comment,
+						DetectionTime: detectionTime,
+						ShodanInfo:    shodanInfo,
+					}
+					results = append(results, result)
 				}
-				results = append(results, result)
-				break // Exit the loop as honeypot is detected
 			}
 		}
 
-		if !honeypotDetected {
-			result := DetectionResult{
+		if len(results) == 0 {
+			results = append(results, DetectionResult{
 				Host:          host,
 				Port:          port,
 				IsHoneypot:    false,
-				HoneypotType:  "",
-				Confidence:    "",
-				Comment:       "",
 				DetectionTime: detectionTime,
-			}
-			results = append(results, result)
+				ShodanInfo:    shodanInfo,
+			})
 		}
 	}
 	return results
 }
 
-func sendRequest(host string, port int, step Step, timeout int) (net.Conn, error) {
-	address := fmt.Sprintf("%s:%d", host, port)
-	conn, err := net.DialTimeout(*proto, address, time.Duration(timeout)*time.Second)
-	if err != nil {
-		if *verbose {
-			fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Verbose] Error dialing %s: %s", address, err)))
-		}
-		return nil, err
-	}
-
+func sendRequest(conn net.Conn, step Step, timeout int) error {
 	conn.SetReadDeadline(time.Now().Add(time.Duration(timeout) * time.Second))
 
 	var request []byte
 
-	if *verbose {
-		fmt.Println(color.Ize(color.Cyan, fmt.Sprintf("[Verbose] Step info: InputType=%s, Input=%s", step.InputType, step.Input)))
+	if *debug {
+		fmt.Println(color.Ize(color.Cyan, fmt.Sprintf("[Debug] Step info: InputType=%s, Input=%s", step.InputType, step.Input)))
 	}
 	switch step.InputType {
 	case "string":
 		request = []byte(step.Input + "\n")
 	case "hex":
+		var err error
 		request, err = hex.DecodeString(step.Input)
 		if err != nil {
-			if *verbose {
-				fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Verbose] Error decoding hex string: %s", err)))
+			if *debug {
+				fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Debug] Error decoding hex string: %s", err)))
 			}
-			return nil, err
+			return err
 		}
 	case "GET", "POST":
-		request = []byte(fmt.Sprintf("%s / HTTP/1.1\r\nHost: %s\r\n\r\n", step.Input, host))
+		request = []byte(fmt.Sprintf("%s / HTTP/1.1\r\nHost: %s\r\n\r\n", step.Input, conn.RemoteAddr().String()))
 	default:
 		errMsg := fmt.Sprintf("unsupported input type: %s", step.InputType)
-		if *verbose {
-			fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Verbose] %s", errMsg)))
+		if *debug {
+			fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Debug] %s", errMsg)))
 		}
-		return nil, fmt.Errorf(errMsg)
+		return fmt.Errorf(errMsg)
 	}
 
-	if *verbose {
-		fmt.Println(color.Ize(color.Green, fmt.Sprintf("[Verbose] Sending request to %s", address)))
+	if *debug {
+		fmt.Println(color.Ize(color.Yellow, "[Debug] Sending request"))
 	}
 
-	_, err = conn.Write(request)
+	_, err := conn.Write(request)
 	if err != nil {
-		if *verbose {
-			fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Verbose] Error sending request: %s", err)))
+		if *debug {
+			fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Debug] Error sending request: %s", err)))
 		}
-		return nil, err
+		return err
 	}
 
-	if *verbose {
-		fmt.Println(color.Ize(color.Green, "[Verbose] Request sent successfully"))
+	if *debug {
+		fmt.Println(color.Ize(color.Yellow, "[Verbose] Request sent successfully"))
 	}
 
-	return conn, nil
+	return nil
 }
 
 func readResponse(conn net.Conn) (string, error) {
-	buffer := make([]byte, 4096)
-	n, err := conn.Read(buffer)
-	if err != nil {
-		if *verbose {
-			fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Verbose] Error reading response: %s", err)))
+	var response strings.Builder
+	buffer := make([]byte, 16048)
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			if err != io.EOF {
+				if *debug {
+					fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Debug] Error reading response: %s", err)))
+				}
+				return "", err
+			}
+			break
 		}
-		return "", err
-	}
-	response := string(buffer[:n])
 
-	if *verbose {
-		fmt.Println(color.Ize(color.Green, fmt.Sprintf("[Verbose] Response received: %s", response)))
+		response.Write(buffer[:n])
+
+		if *debug {
+			fmt.Println(color.Ize(color.Purple, fmt.Sprintf("[Debug] Partial Response received: %s", string(buffer[:n]))))
+		}
+
+		if n < len(buffer) {
+			break
+		}
 	}
 
-	return response, nil
+	fullResponse := response.String()
+	if *debug {
+		fmt.Println(color.Ize(color.Green, fmt.Sprintf("[Debug] Full Response received: %s", fullResponse)))
+	}
+
+	return fullResponse, nil
 }
-
-func isResponseMatch(response, matchType, matchPattern string) bool {
+func isResponseMatch(response, matchType, matchPattern string, invertMatch bool) bool {
 	trimmedResponse := strings.TrimSpace(response)
 
-	if *verbose {
-		fmt.Println(color.Ize(color.Blue, fmt.Sprintf("[Verbose] Trimmed Response: %s", trimmedResponse)))
-		fmt.Println(color.Ize(color.Blue, fmt.Sprintf("[Verbose] Expected Output: %s %s", matchPattern, matchType)))
+	if *debug {
+		fmt.Println(color.Ize(color.Purple, fmt.Sprintf("[Debug] Trimmed Response: %s", trimmedResponse)))
+		fmt.Println(color.Ize(color.Purple, fmt.Sprintf("[Debug] Match Type: %s", matchType)))
+		fmt.Println(color.Ize(color.Purple, fmt.Sprintf("[Debug] Expected Output Pattern: %s", matchPattern)))
 	}
 
+	match := false
 	switch matchType {
 	case "string":
-		if *verbose {
-			fmt.Println(color.Ize(color.Blue, fmt.Sprintf("[Verbose] Comparing string: Response contains Output? %t", strings.Contains(trimmedResponse, matchPattern))))
+		match = strings.Contains(trimmedResponse, matchPattern)
+		if *debug {
+			fmt.Println(color.Ize(color.Green, fmt.Sprintf("[Debug] Comparing string: Response contains Output? %t", match)))
 		}
-		return strings.Contains(trimmedResponse, matchPattern)
 	case "no-response":
-		return response == "" || response == "<no-response-indicator>"
+		match = response == "" || response == "<no-response-indicator>"
 	case "hex":
 		hexResponse := hex.EncodeToString([]byte(trimmedResponse))
-		return strings.Contains(hexResponse, matchPattern)
+		match = strings.Contains(hexResponse, matchPattern)
 	case "not-equals":
-		return response != matchPattern
+		match = response != matchPattern
 	case "regex":
-		matched, err := regexp.MatchString(matchPattern, trimmedResponse)
+		var err error
+		match, err = regexp.MatchString(matchPattern, trimmedResponse)
 		if err != nil {
-			return false
+			if *debug {
+				fmt.Println(color.Ize(color.Red, fmt.Sprintf("[Debug] Regex match error: %s", err)))
+			}
+			match = false
 		}
-		return matched
+		if *debug {
+			fmt.Println(color.Ize(color.Purple, fmt.Sprintf("[Debug] Regex match result: %t", match)))
+		}
 	default:
-		return false
+		if *debug {
+			fmt.Println(color.Ize(color.Red, "[Debug] Unknown match type"))
+		}
+		match = false
 	}
+
+	if invertMatch {
+		if *debug {
+			fmt.Println(color.Ize(color.Yellow, "[Debug] Inverting match as inverMatch set on rule"))
+		}
+		return !match
+	}
+	return match
 }
